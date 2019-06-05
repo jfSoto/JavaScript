9a6c5843517df6d3b9e683c8145883d5ba9840cd
jfSoto <35599612+jfSoto@users.noreply.github.com>
2019-06-05 12:48:47 +0200

    Create Ejercicio1.js

---
+++ Basic Javascript/Práctica/Ejercicio1/Ejercicio1.js
@@ -0,0 +1,15 @@
+var cad = "GrupoStudium"
+
+String.prototype.separar_caracteres = function(cadena)
+{
+    var array1 = this.split("")
+    var array2 = []
+    for (var i=0; i < this.length -1; i++) 
+    {
+        array2 += array1[i] + cadena
+    }
+    array2 += array1[i]
+    return array2
+}
+
+console.log(cad.separar_caracteres("*"))
