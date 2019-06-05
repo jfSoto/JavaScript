69df3e016b51296666619d5b7f06b6186d0afae4
jfSoto <35599612+jfSoto@users.noreply.github.com>
2019-06-05 12:35:24 +0200

    Create ejercicio.js

---
+++ Basic Javascript/Ejercicio2/ejercicio.js
@@ -0,0 +1,9 @@
+/*EJERCICIO 1
+var frase = prompt("Escriba una frase: ");
+var nueva_ventana = window.open()
+nueva_ventana.document.write("<h1>"+frase+"</h1>") */
+
+//EJERCICIO 2
+var anchura = screen.width
+var altura = screen.height
+window.open("https://grupostudium.com/", "", "width="+anchura/2+", height="+altura/2)
