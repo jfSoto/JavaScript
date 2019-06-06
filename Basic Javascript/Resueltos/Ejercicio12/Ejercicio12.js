a5266796303efeb480109046f5c176a89986b2a1
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 13:59:15 +0200

    Create Ejercicio12.js

---
+++ Basic Javascript/Resueltos/Ejercicio12/Ejercicio12.js
@@ -0,0 +1,6 @@
+// Modificar el ejercicio anterior para que el tamaño de la nueva ventana sea la
+// mitad del tamaño de la ventana donde se está ejecutando el código.
+
+var width = screen.width / 2
+var height = screen.height / 2
+var nueva_ventana = window.open('https://grupostudium.com', '', 'width=' + width + ', height = '+height)
