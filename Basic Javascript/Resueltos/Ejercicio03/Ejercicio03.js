2ebf2228f53681ebc3af316d287d314f92e32166
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 13:40:23 +0200

    Create Ejercicio03.js

---
+++ Basic Javascript/Resueltos/Ejercicio03/Ejercicio03.js
@@ -0,0 +1,12 @@
+// Hacer un programa que pida un número al usuario e indicar si es par o impar.
+
+var numero = prompt("Escribe un número");
+
+if (numero % 2 == 0) 
+{
+    alert("El número es par");
+} 
+else 
+{
+    alert("El número es impar");
+}
