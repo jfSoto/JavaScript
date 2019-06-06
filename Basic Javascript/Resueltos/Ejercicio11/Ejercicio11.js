616e97e62af3224ebbd4c066e3f052fcf7800e0d
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 13:58:17 +0200

    Create Ejercicio11.js

---
+++ Basic Javascript/Resueltos/Ejercicio11/Ejercicio11.js
@@ -0,0 +1,4 @@
+// Modificar el ejercicio anterior para que en una nueva ventana de 300x300
+// aparezca la página web https://grupostudium.com .
+
+window.open('https://grupostudium.com', 'Nueva Ventana', 'width=300,height=300')
