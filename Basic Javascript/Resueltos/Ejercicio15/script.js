c53bc73836a86ef66703d0896e1258187ca27d6b
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 14:19:20 +0200

    Update script.js

--- Basic Javascript/Resueltos/Ejercicio15/script.js
+++ Basic Javascript/Resueltos/Ejercicio15/script.js
@@ -4,7 +4,8 @@ var numero2 = prompt("Escribe un número")
 document.getElementById('suma').onclick = function () 
 {
     var nuevaVentana = window.open()
-    var suma = numero1 + numero2
 
-    nuevaVentana.document.write("La suma de " + numero1 + " y " + numero2 + " es " + (suma));
+    nuevaVentana.document.write("La suma de " + numero1 + " y " + numero2 + " es " + (numero1 + numero2));
 }
+
+// El resto de métodos es igual
